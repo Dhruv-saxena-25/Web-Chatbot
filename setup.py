fa3882ea367dc70470d519f1dd4a24f53a7f996d
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-09-06 19:26:11 +0530

    setup workflows

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="web-chatbot",
+    version="0.0.1",
+    author="Dhruv",
+    author_email="saxena25dhruv@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
